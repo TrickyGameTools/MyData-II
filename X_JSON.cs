88602135195e30c0da304384bf33d9f1b8bd466a
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-08-19 23:53:23 +0200

    Update X_JSON.cs

--- X_JSON.cs
+++ X_JSON.cs
@@ -31,81 +31,81 @@ using System.Threading.Tasks;
 using TrickyUnits;
 
 namespace MyData_II {
-    internal class X_JSON : Export {
+	internal class X_JSON : Export {
 
-        public X_JSON() {
-            MKL.Version("MyData II - X_JSON.cs","23.08.19");
-            MKL.Lic    ("MyData II - X_JSON.cs","GNU General Public License 3");
-        }
+		public X_JSON() {
+			MKL.Version("MyData II - X_JSON.cs","23.08.19");
+			MKL.Lic    ("MyData II - X_JSON.cs","GNU General Public License 3");
+		}
 
-        public override string XRecord(MyData MyDataBase, string recname = "", bool addreturn = false) {
-            var t = ""; if (!addreturn) t = "\t";
-            var ret = t + "{" + eol;
-            var first = true;
-            foreach (string k in MyDataBase.Record(recname).Keys) {
-                var val = MyDataBase[recname, k];
-                var ok = !(MyDataBase.Sys_RemoveNonExistent && (val == "" || (MyDataBase.Fields[k].Type.ToString().ToLower() == "bool" && val.ToUpper() != "TRUE")));
-                var lin = $"\t{t}\"{k}\" : ";
-                if (!ok) { lin += "null"; } else {
-                    switch (MyDataBase.Fields[k].Type.ToString().ToLower()) { // Lazy! 
-                        case "string":
-                        case "mc":
-                            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(MyDataBase[recname, k]);
-                            lin += "\"";
-                            foreach (byte b in bytes) {
-                                if ((b > 31 && b < 128) && b != '"') { lin += qstr.Chr(b); } else {
-                                    lin += "\\" + qstr.Right("00" + Convert.ToString(b, 8), 3);
-                                }
-                            }
-                            lin += "\"";
-                            break;
-                        case "int":
-                        case "double":
-                            lin += MyDataBase[recname, k];
-                            break;
-                        case "bool":
-                            if (MyDataBase[recname, k].ToUpper() == "TRUE") lin += "true"; else lin += "false";
-                            break;
-                        case "date":
-                            var sd = MyDataBase[recname, k].Split('/');
-                            lin += "{" + $" \"day\" : {sd[0]}, \"month\" : {sd[1]}, \"year\" : {sd[2]} " + "}";
-                            break;
-                        case "time":
-                            var si = TimeSplit(MyDataBase[recname, k]);
-                            lin += "{" + $" \"hour\" : {si[0]}, \"minute\" : {si[1]}, \"second\" : {si[2]} " + "}";
-                            break;
-                        case "color":
-                            si = TimeSplit(MyDataBase[recname, k], ',');
-                            lin += "{" + $" \"red\" : {si[0]}, \"green\" : {si[1]}, \"blue\" : {si[2]} " + "}";
-                            break;
-                        default:
-                            Error.Err($"I cannot handle type -> {MyDataBase.Fields[k].Type}");
-                            break;
-                    }
-                }
-                if (ok || (!MyDataBase.Sys.ContainsKey("NONULL") || MyDataBase.Sys["NONULL"].ToUpper() != "TRUE")) {
-                    if (!first) ret += $",{eol}"; first = false;
-                    ret += lin;
-                }
+		public override string XRecord(MyData MyDataBase, string recname = "", bool addreturn = false) {
+			var t = ""; if (!addreturn) t = "\t";
+			var ret = t + "{" + eol;
+			var first = true;
+			foreach (string k in MyDataBase.Record(recname).Keys) {
+				var val = MyDataBase[recname, k];
+				var ok = !(MyDataBase.Sys_RemoveNonExistent && (val == "" || (MyDataBase.Fields[k].Type.ToString().ToLower() == "bool" && val.ToUpper() != "TRUE")));
+				var lin = $"\t{t}\"{k}\" : ";
+				if (!ok) { lin += "null"; } else {
+					switch (MyDataBase.Fields[k].Type.ToString().ToLower()) { // Lazy! 
+						case "string":
+						case "mc":
+							byte[] bytes = System.Text.Encoding.ASCII.GetBytes(MyDataBase[recname, k]);
+							lin += "\"";
+							foreach (byte b in bytes) {
+								if ((b > 31 && b < 128) && b != '"') { lin += qstr.Chr(b); } else {
+									lin += "\\" + qstr.Right("00" + Convert.ToString(b, 8), 3);
+								}
+							}
+							lin += "\"";
+							break;
+						case "int":
+						case "double":
+							lin += MyDataBase[recname, k];
+							break;
+						case "bool":
+							if (MyDataBase[recname, k].ToUpper() == "TRUE") lin += "true"; else lin += "false";
+							break;
+						case "date":
+							var sd = MyDataBase[recname, k].Split('/');
+							lin += "{" + $" \"day\" : {sd[0]}, \"month\" : {sd[1]}, \"year\" : {sd[2]} " + "}";
+							break;
+						case "time":
+							var si = TimeSplit(MyDataBase[recname, k]);
+							lin += "{" + $" \"hour\" : {si[0]}, \"minute\" : {si[1]}, \"second\" : {si[2]} " + "}";
+							break;
+						case "color":
+							si = TimeSplit(MyDataBase[recname, k], ',');
+							lin += "{" + $" \"red\" : {si[0]}, \"green\" : {si[1]}, \"blue\" : {si[2]} " + "}";
+							break;
+						default:
+							Error.Err($"I cannot handle type -> {MyDataBase.Fields[k].Type}");
+							break;
+					}
+				}
+				if (ok || (!MyDataBase.Sys.ContainsKey("NONULL") || MyDataBase.Sys["NONULL"].ToUpper() != "TRUE")) {
+					if (!first) ret += $",{eol}"; first = false;
+					ret += lin;
+				}
 
-            }
-            ret += "}" + eol;
-            return ret;
-        }
+			}
+			ret += "}" + eol;
+			return ret;
+		}
 
-        public override string XBase(MyData MyDataBase) {
-            var ret = "{" + eol;
-            var first = true;
-            foreach (string rID in MyDataBase.Records.Keys) {
-                if (!first) ret += $",{eol}"; first = false;
-                ret += $"\t\"{rID}\" : {XRecord(MyDataBase, rID, false)}";
-            }
-            ret += "}" + eol + eol;
-            return ret;
+		public override string XBase(MyData MyDataBase) {
+			var ret = "{" + eol;
+			var first = true;
+			foreach (string rID in MyDataBase.Records.Keys) {
+				if (!first) ret += $",{eol}"; first = false;
+				ret += $"\t\"{rID}\" : {XRecord(MyDataBase, rID, false)}";
+			}
+			ret += "}" + eol + eol;
+			return ret;
 
-        }
+		}
 
-        public override string Extension(MyData database) => "json";
-    }
+		public override string Extension(MyData database) => "json";
+	}
 
 }
